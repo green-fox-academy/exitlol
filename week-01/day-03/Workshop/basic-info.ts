8a10b7e23d0b2fd7f33e00e2accf0527de841d92
sandorbene <neutralpointz@gmail.com>
2018-05-10 14:03:15 +0200

    Added basic-info logged all my info in new lines, also used an if-else statement for deciding marriage

---
+++ week-01/day-03/Workshop/basic-info.ts
@@ -0,0 +1,17 @@
+export{};
+'use strict';
+
+let myName: string = 'Sándor Bene';
+let myAge: number = 26;
+let myHeight: number = 1.78;
+let marriage: boolean = false;
+
+console.log(`My name is ${myName}\nAnd I'm ${myAge} years old\nAlso I'm ${myHeight} meters tall`)
+
+if (marriage == false) {
+  console.log('I\'m not married. Yet!');
+} else {
+  console.log('And I\'m happily married.');
+}
+
+//Printing out my variables
