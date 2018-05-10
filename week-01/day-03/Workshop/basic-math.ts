4301868de36e77d7768755eefdfb4549db6468c4
sandorbene <neutralpointz@gmail.com>
2018-05-10 11:45:52 +0200

    Added the basic math exercise, everything is stored in values and those are being logged

---
+++ week-01/day-03/Workshop/basic-math.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+let numOne: number = 22;
+let numTwo: number = 13;
+
+let addition = (numOne + numTwo);
+console.log(addition);
+
+let substraction = (numOne - numTwo);
+console.log(substraction);
+
+let multiplication = (numOne * numTwo);
+console.log(multiplication);
+
+let division = (numOne / numTwo);
+console.log(division);
+
+let remainder = (numOne % numTwo);
+console.log(remainder);
