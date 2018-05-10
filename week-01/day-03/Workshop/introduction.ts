0b413b875ebf107d16bab45728c0b005d8e6793a
sandorbene <neutralpointz@gmail.com>
2018-05-10 11:36:55 +0200

    Done with introduction exercise, also used \n instead of multiple console.logging

---
+++ week-01/day-03/Workshop/introduction.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+let myName: string = 'Sándor Bene'
+let myAge: number = 26;
+let myHeight: number = 1.75;
+
+console.log(`Hy my name is ${myName}\nI\'m ${myAge} years old\nMy height is: ${myHeight} meters`);
