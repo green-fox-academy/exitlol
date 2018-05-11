03b2b2112221fb8e73eefcf508b3977f63b620f5
sandorbene <neutralpointz@gmail.com>
2018-05-11 11:44:25 +0200

    Added the biggest pain in the ass so far, draw pyramid with *s

---
+++ week-01/day-03/Workshop/pyramid.ts
@@ -0,0 +1,27 @@
+export { };
+'use strict';
+
+// Write a program that draws a
+// pyramid like this:
+//
+//    *      4 space 1 star   * spacecount -1 per cycle
+//   ***     3 space 3 star
+//  *****    2 space 5 star
+// *******   1 space 7 star 
+//
+// The pyramid should have as many lines as lineCount is
+
+let lineCount: number = 4;
+let star: string = '*';
+
+for (let i: number = lineCount; i >= 0; i--) {
+  let spaces: string = '';
+  // console.log(`Spacecount is now: ${spaceCount}`);
+  for (let j: number = 0; j <= i; j++) {
+    spaces += ' ';
+  }
+  console.log(`${spaces}${star}`);
+  star += '**';
+}
+
+
