91dcf9cf6789334e3f06fecedbbb552f3cff36c8
sandorbene <neutralpointz@gmail.com>
2018-05-14 11:34:59 +0200

    Added greeter.ts

---
+++ week-02/day-01/Workshop/greeter.ts
@@ -0,0 +1,13 @@
+export{};
+'use strict';
+
+// -  Create variable named `name` and assign the value `Greenfox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Greenfox`
+//     -  Prepare for the special case when no parameters are given
+// -  Greet `name`
+
+function greet(name: string = 'Greenfox') {
+  console.log(`Greetings ${name}!`);
+}
+greet();
