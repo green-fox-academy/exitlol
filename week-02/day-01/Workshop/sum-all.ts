49bb13f850c187464ee845a56606068ebe7cd917
sandorbene <neutralpointz@gmail.com>
2018-05-14 14:43:40 +0200

    Added sum-all.ts

---
+++ week-02/day-01/Workshop/sum-all.ts
@@ -0,0 +1,13 @@
+export {};
+'use strict';
+
+// -  Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
+// -  Log the sum of the elements in `ai` to the console
+
+let ai = [3, 4, 5, 6, 7];
+let sum: number = 0;
+
+ai.map(arrayVal => {
+  sum += arrayVal;
+});
+console.log(sum);
