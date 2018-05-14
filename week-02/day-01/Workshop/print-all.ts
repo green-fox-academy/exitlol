c30ecfda62251bda128716d31f17649c1e6de6e4
sandorbene <neutralpointz@gmail.com>
2018-05-14 13:20:00 +0200

    Added print-all.ts

---
+++ week-02/day-01/Workshop/print-all.ts
@@ -0,0 +1,13 @@
+export{};
+'use strict';
+
+// -  Create a variable named `integers` with the following content: `[4, 5, 6, 7]`
+// -  Log each the element of `integers` to the console*
+// *hint: use a loop, console.log(integers) won't cut it
+// -  bonus for using the correct built in array method
+
+const integers = [4, 5, 6, 7];
+
+for (let i: number = 0; i < integers.length; i++) {
+  console.log(integers[i]);
+}
