52ee30933bddb5644a5fadab218540e0042793ff
sandorbene <neutralpointz@gmail.com>
2018-05-16 08:14:57 +0200

    Added types after declaration of iterator in the loop, also added 3= instead of 2 in statements

--- week-01/day-03/Workshop/fizz-buzz.ts
+++ week-01/day-03/Workshop/fizz-buzz.ts
@@ -9,12 +9,12 @@ let fizz: string = 'Fizz';
 let buzz: string = 'Buzz';
 
 //console.log(`${fizz}${buzz}`);
-for (let i = 1; i < 101; i++) {
-  if (i % 3 == 0 && i % 5 ==0) {
+for (let i: number = 1; i < 101; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
     console.log(`${fizz}${buzz}`);
-  } else if (i % 5 == 0) {
+  } else if (i % 5 === 0) {
     console.log(buzz);
-  } else if (i % 3 == 0) {
+  } else if (i % 3 === 0) {
     console.log(fizz);
   }
 }
