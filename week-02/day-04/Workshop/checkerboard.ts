193f7a0142a60cbc4fee2ee3af8fadb5fbc2b07c
sandorbene <neutralpointz@gmail.com>
2018-05-17 15:51:48 +0200

    Added checkerboard.ts exercise

---
+++ week-02/day-04/Workshop/checkerboard.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Fill the canvas with a checkerboard pattern.
+
+function drawBoard(x,y,size) {
+  for (let i: number = 0; i < 15; i++) {
+    for (let j: number = 0; j < 15; j++) {
+      if (i % 2 === j % 2) {
+        ctx.fillStyle = 'black';
+        ctx.fillRect(x * i, y * j, size, size);
+      } else {
+        ctx.fillStyle = 'white';
+        ctx.fillRect(x * i, y * j, size, size);
+      }
+    }
+  }
+}
+
+drawBoard(50,50,50);
