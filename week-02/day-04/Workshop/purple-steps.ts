f4aa434c3230cd3213312987b7da706ac4260afa
sandorbene <neutralpointz@gmail.com>
2018-05-17 13:43:45 +0200

    Added purple-steps.ts with not so purple colors

---
+++ week-02/day-04/Workshop/purple-steps.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Reproduce this:
+// [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+
+function drawRect (size) {
+  for(let i: number = 0; i < 58; i++) {
+    ctx.fillStyle = '#53c237';
+    // if you don't want to do diagonal just remove the *400/600 from below and modify the loop iteration value (58)
+    ctx.fillRect(size+i*10,((size+i*10)*(400/600)),size,size);
+  }
+}
+
+drawRect(10);
