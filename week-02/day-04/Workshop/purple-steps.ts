503687da27e7475de04a0a7e88a582aec716c388
sandorbene <neutralpointz@gmail.com>
2018-05-17 13:45:48 +0200

    Added strokeRect for better visibility

--- week-02/day-04/Workshop/purple-steps.ts
+++ week-02/day-04/Workshop/purple-steps.ts
@@ -11,7 +11,9 @@ function drawRect (size) {
   for(let i: number = 0; i < 58; i++) {
     ctx.fillStyle = '#53c237';
     // if you don't want to do diagonal just remove the *400/600 from below and modify the loop iteration value (58)
+    ctx.strokeStyle = 'black';
     ctx.fillRect(size+i*10,((size+i*10)*(400/600)),size,size);
+    ctx.strokeRect(size+i*10,((size+i*10)*(400/600)),size,size);
   }
 }
 
