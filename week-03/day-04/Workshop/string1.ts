2c3274dc90a2d6b8f8009fcd5dd3436986a72e77
sandorbene <neutralpointz@gmail.com>
2018-05-24 13:43:45 +0200

    Added string1.ts exercise

---
+++ week-03/day-04/Workshop/string1.ts
@@ -0,0 +1,19 @@
+'use strict';
+export{};
+
+// Given a string, compute recursively (no loops) a new string where all the
+// lowercase 'x' chars have been changed to 'y' chars.
+
+let replaceVal: string = 'y';
+
+function strReplacer(string: string) {
+let newStr: string = string.replace('x','y');
+  if(string.indexOf('x') === -1) {
+    return newStr;
+  } else {
+    return strReplacer(newStr);
+  }
+}
+
+console.log(strReplacer('0xXnXL5cx6NxlXxZrqYX'));
+console.log(strReplacer('xxXxXXXXxXxxxxXXXXXxXXX'));
