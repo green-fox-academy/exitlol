5593db90f8d9a6f2369f8db567cdaf6e71568fe5
sandorbene <neutralpointz@gmail.com>
2018-05-29 08:12:36 +0200

    Added missing ; from console.log statements

--- week-02/day-01/Workshop/colors.ts
+++ week-02/day-01/Workshop/colors.ts
@@ -15,6 +15,6 @@ let colors = [
   ['orchid', 'violet', 'pink', 'hot pink']
 ];
 
-console.log(colors[0])
-console.log(colors[1])
-console.log(colors[2])
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
