70ae8993fc79ad6c8b281932a0da44861b0d3eeb
sandorbene <neutralpointz@gmail.com>
2018-05-29 08:16:06 +0200

    Fixxed spaces according to styleguide, and added an extra line before function declaration

--- week-02/day-02/Workshop/append-letter.ts
+++ week-02/day-02/Workshop/append-letter.ts
@@ -1,11 +1,12 @@
-export{};
+export { };
 'use strict';
 // Create a function called "appendA()" that adds "a" to every string in the far list.
 // The parameter should be a list.
 let far: string[] = ['kuty', 'macsk', 'kacs', 'rók', 'halacsk'];
+
 function appendA(far) {
   for (let i: number = 0; i < far.length; i++) {
-    far[i] = far[i]+'a';
+    far[i] = far[i] + 'a';
   }
   return far;
 }
