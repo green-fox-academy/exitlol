afbbce86d6a500ebc0ddeaede5953f5cb8710f2b
sandorbene <neutralpointz@gmail.com>
2018-05-30 08:00:29 +0200

    Fixxed code(Ln49,col28, missing = sign )

--- week-03/day-03/Workshop/dice-set.ts
+++ week-03/day-03/Workshop/dice-set.ts
@@ -4,37 +4,37 @@ class DiceSet {
   // Check the current rolled numbers with getCurrent()
   // You can reroll with reroll()
   // Your task is to roll the dices until all of the dices are 6
-      dices: number[];
-      readonly numOfDices: number = 6;
-  
-      roll(): number[] {
-          this.dices = [];
-          for(var i = 0; i < this.numOfDices; i++) { 
-              this.dices.push(Math.floor(Math.random() * 6 + 1));
-          }
-          return this.dices;
-      }
-  
-      reroll(index?: number) {
-          if(index === undefined) {
-              for(var i = 0; i < this.numOfDices; i++) { 
-                  this.dices[i] = Math.floor(Math.random() * 6 + 1);
-              }
-          } else {
-              this.dices[index] = Math.floor(Math.random() * 6 + 1);
-          }
-      }
-      
-      getCurrent(index?: number) {
-          if(index == undefined) {
-              for(var i = 0; i < this.numOfDices; i++) { 
-                  console.log(this.dices[i]);
-              }
-          } else {
-              console.log(this.dices[index]);
-          }        
-      }
+  dices: number[];
+  readonly numOfDices: number = 6;
+
+  roll(): number[] {
+    this.dices = [];
+    for (var i = 0; i < this.numOfDices; i++) {
+      this.dices.push(Math.floor(Math.random() * 6 + 1));
+    }
+    return this.dices;
   }
+
+  reroll(index?: number) {
+    if (index === undefined) {
+      for (var i = 0; i < this.numOfDices; i++) {
+        this.dices[i] = Math.floor(Math.random() * 6 + 1);
+      }
+    } else {
+      this.dices[index] = Math.floor(Math.random() * 6 + 1);
+    }
+  }
+
+  getCurrent(index?: number) {
+    if (index == undefined) {
+      for (var i = 0; i < this.numOfDices; i++) {
+        console.log(this.dices[i]);
+      }
+    } else {
+      console.log(this.dices[index]);
+    }
+  }
+}
 
 
 
@@ -44,9 +44,9 @@ diceSet.roll();
 diceSet.getCurrent();
 console.log('------------------');
 
-while (!diceSet.dices.every (element => {return element === 6;})) {
+while (!diceSet.dices.every(element => { return element === 6; })) {
   for (let i: number = 0; i < diceSet.numOfDices; i++) {
-    if(diceSet.dices[i] != 6) {
+    if (diceSet.dices[i] !== 6) {
       diceSet.reroll(i);
     }
   }
