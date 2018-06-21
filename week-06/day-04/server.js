605de1c8996736568235904be9eb7ff35fd3ff18
sandorbene <neutralpointz@gmail.com>
2018-06-21 11:23:16 +0200

    Added mat review about servers

--- week-06/day-04/server.js
+++ week-06/day-04/server.js
@@ -1,11 +1,23 @@
 "use strict";
 
 const express = require("express");
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
+app.use('/static', express.static('static'));
+
 app.get("/", (req, res) => {
-  res.send("Hello world!!!!!!!");
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.get("/greet/:message", (req, res) => {
+  console.log(req.query);
+  let name = "Fox";
+  if (req.query.name) {
+    name = req.query.name;
+  }
+  res.send(`${req.params.message}, ${name}`);
 });
 
 app.listen(PORT, () => {
