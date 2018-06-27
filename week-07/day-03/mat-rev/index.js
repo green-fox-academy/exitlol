aa702fa214cde1b1ad70551a9e6b6923b7e5f4d4
sandorbene <neutralpointz@gmail.com>
2018-06-27 10:59:52 +0200

    Complete mat-review with hidden credintials

--- week-07/day-03/mat-rev/index.js
+++ week-07/day-03/mat-rev/index.js
@@ -1,14 +1,41 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const PORT = 3000;
 
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+
+
 app.get('/test', (req, res) => {
-  res.json({
-    message: 'OK',
+  let sql = 'SELECT * from author;';
+  let queryInputs = [];
+
+  if (req.query.country) {
+    sql = 'SELECT * from author WHERE country = ?;';
+    queryInputs = [req.query.country];
+  }
+
+  connection.query(sql, queryInputs, (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+      return;
+    }
+    res.json({
+      authors: rows,
+    });
   });
 });
+
 app.listen(PORT, () => {
   console.log(`The server is up and running on: ${PORT}`);
 });
