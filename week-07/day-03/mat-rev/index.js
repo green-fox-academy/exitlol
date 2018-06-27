160f4fbb21a5254173920d0082fa8b789ac4e6e9
sandorbene <neutralpointz@gmail.com>
2018-06-27 09:54:58 +0200

    Added folder for day-03 with mat-review

---
+++ week-07/day-03/mat-rev/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/test', (req, res) => {
+  res.json({
+    message: 'OK',
+  });
+});
+app.listen(PORT, () => {
+  console.log(`The server is up and running on: ${PORT}`);
+});
