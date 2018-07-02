f8eb4f38f8217f055bf9a1f2d2236ff2b3298be0
sandorbene <neutralpointz@gmail.com>
2018-07-02 17:41:29 +0200

    Added pair funcionality && test for that

--- week-08/day-01/poker.test.js
+++ week-08/day-01/poker.test.js
@@ -4,8 +4,8 @@ const test = require('tape');
 const poker = require('./poker');
 
 test('test single cards', t => {
-  const p1 = ['ST', 'SQ', 'CA', 'D5', 'H6'];
-  const p2 = ['C3', 'CT', 'D5', 'H6', 'SK'];
+  const p1 = ['CT', 'CK', 'CQ', 'DA', 'H6'];
+  const p2 = ['C3', 'C4', 'D7', 'H2', 'S5'];
 
   t.equal(poker(p1, p2), 'p1');
   t.end();
