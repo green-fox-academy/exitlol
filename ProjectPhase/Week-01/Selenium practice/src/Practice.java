071d73dd4903bc10a1fd5d608a54686a678ac9f6
exitlol <neutralpointz@gmail.com>
2018-08-02 11:31:54 +0200

    FEAT(add and count todos)

--- ProjectPhase/Week-01/Selenium practice/src/Practice.java
+++ ProjectPhase/Week-01/Selenium practice/src/Practice.java
@@ -1,6 +1,48 @@
-public class Practice {
-    public static void main(String[] args) {
-    
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
+public class Practice {
+
+  WebDriver driver;
+
+  public void callBrowser() {
+    try {
+      System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+      driver = new ChromeDriver();
+      driver.manage().deleteAllCookies();
+      driver.manage().window().maximize();
+      driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+      driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+
+      driver.get("http://todomvc.com/examples/vanillajs/");
+      addToDo();
+      countToDo();
+
+
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+
+  }
+
+  public void addToDo() {
+    driver.findElement(By.xpath("/html/body/section/header/input")).sendKeys("Arrive in time", Keys.ENTER);
+    driver.findElement(By.xpath("/html/body/section/header/input")).sendKeys("Practice Java & Selenium", Keys.ENTER);
+    driver.findElement(By.xpath("/html/body/section/header/input")).sendKeys("Practice more", Keys.ENTER);
+  }
+
+  public void countToDo() {
+    WebElement counter = driver.findElement(By.xpath("/html/body/section/footer/span/strong"));
+    String counterText = counter.getText();
+    System.out.println(counterText);
+
+  }
+
+  public static void main(String[] args) {
+    Practice myObj = new Practice();
+    myObj.callBrowser();
+
+  }
 }
