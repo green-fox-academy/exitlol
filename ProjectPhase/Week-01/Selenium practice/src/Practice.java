8064f6faef3f4b1150f7cb19f08ef46279f6436a
exitlol <neutralpointz@gmail.com>
2018-08-02 12:22:50 +0200

    FEAT(edit todo running, but nor Working as intended -- missing double click)

--- ProjectPhase/Week-01/Selenium practice/src/Practice.java
+++ ProjectPhase/Week-01/Selenium practice/src/Practice.java
@@ -1,16 +1,19 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.concurrent.TimeUnit;
 
 public class Practice {
 
   WebDriver driver;
+  Actions action;
 
   public void callBrowser() {
     try {
       System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
       driver = new ChromeDriver();
+      action = new Actions(driver);
       driver.manage().deleteAllCookies();
       driver.manage().window().maximize();
       driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
@@ -19,6 +22,7 @@ public class Practice {
       driver.get("http://todomvc.com/examples/vanillajs/");
       addToDo();
       countToDo();
+      editToDo();
 
 
     } catch (Exception e) {
@@ -28,7 +32,7 @@ public class Practice {
   }
 
   public void addToDo() {
-    driver.findElement(By.xpath("/html/body/section/header/input")).sendKeys("Arrive in time", Keys.ENTER);
+    driver.findElement(By.xpath("/html/body/section/header/input")).sendKeys("Arrive in thime", Keys.ENTER);
     driver.findElement(By.xpath("/html/body/section/header/input")).sendKeys("Practice Java & Selenium", Keys.ENTER);
     driver.findElement(By.xpath("/html/body/section/header/input")).sendKeys("Practice more", Keys.ENTER);
   }
@@ -37,7 +41,11 @@ public class Practice {
     WebElement counter = driver.findElement(By.xpath("/html/body/section/footer/span/strong"));
     String counterText = counter.getText();
     System.out.println(counterText);
+  }
 
+  public void editToDo() {
+    WebElement editable = driver.findElement(By.xpath("/html/body/section/section/ul/li[1]/div/label"));
+    action.doubleClick(editable);
   }
 
   public static void main(String[] args) {
