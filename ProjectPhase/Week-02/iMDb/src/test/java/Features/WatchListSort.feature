4cd751b3061220b28b3c9969cedfed6c74ce6b79
exitlol <neutralpointz@gmail.com>
2018-08-08 14:42:36 +0200

    FEAT(watchlist sorting functionality passed)

--- ProjectPhase/Week-02/iMDb/src/test/java/Features/WatchListSort.feature
+++ ProjectPhase/Week-02/iMDb/src/test/java/Features/WatchListSort.feature
@@ -4,10 +4,10 @@ Feature: This deal with the sorting method on IMDb
     Given I navigate to the login page
     And I enter the username as "neutralpointz@gmail.com" and password as "12345678"
     And I click on the login button
-    Then User clicks on watchlist
+    And User clicks on watchlist
+    Then User should see his/her not empty watchlist
 
   Scenario: User is able to sort his/her own watchlist
     Given User is on watchlist page
-    When User clicks on Sort By:
-    And User can sort by "popularity"
+    And User can sort by popularity
     Then User should see the ordered list
--- ProjectPhase/Week-02/iMDb/src/test/java/Steps/WatchListSortSteps.java
+++ ProjectPhase/Week-02/iMDb/src/test/java/Steps/WatchListSortSteps.java
@@ -1,29 +1,44 @@
 package Steps;
 
+import Base.BaseUtil;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
-public class WatchListSortSteps {
+import static java.lang.Integer.parseInt;
+
+public class WatchListSortSteps extends BaseUtil {
+
+  public BaseUtil base;
+
+  public WatchListSortSteps(BaseUtil base) {
+    this.base = base;
+  }
+
   @Given("^User is on watchlist page$")
   public void userIsOnWatchlistPage() throws Throwable {
-    
+    Assert.assertTrue(base.driver.findElement(By.xpath("//*[@id=\"page-1\"]")).isDisplayed());
   }
 
-  @When("^User clicks on Sort By:$")
-  public void userClicksOnSortBy() throws Throwable {
-
-  }
-
-  @And("^User can sort by \"([^\"]*)\"$")
-  public void userCanSortBy(String arg0) throws Throwable {
-
+  @And("^User can sort by popularity$")
+  public void userCanSortBy() throws Throwable {
+    Thread.sleep(1000);
+    Select sorter = new Select(base.driver.findElement(By.xpath("//*[@id=\"center-1-react\"]/div/div[2]/div[1]/div[1]/div[1]/select")));
+    sorter.selectByIndex(2);
   }
 
   @Then("^User should see the ordered list$")
   public void userShouldSeeTheOrderedList() throws Throwable {
-
+    WebElement firstMovie = base.driver.findElement(By.xpath("//*[@id=\"page-1\"]/div/div/div[2]/div[1]/div[1]/span[2]"));
+    WebElement secondMovie = base.driver.findElement(By.xpath("//*[@id=\"center-1-react\"]/div/div[3]/div/div[2]/div/div[2]/div[1]/div[1]/span[2]"));
+    float firstRating = Float.parseFloat(firstMovie.getText());
+    float secondRating = Float.parseFloat(secondMovie.getText());
+    Assert.assertTrue(firstRating > secondRating);
   }
 }
