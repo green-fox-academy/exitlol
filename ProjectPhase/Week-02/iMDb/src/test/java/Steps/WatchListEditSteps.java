ef337a572b18c031c3867d33f71891b31a6aca2e
exitlol <neutralpointz@gmail.com>
2018-08-08 16:24:47 +0200

    FEAT(IMDb edit watchlist test passed)

--- ProjectPhase/Week-02/iMDb/src/test/java/Steps/WatchListEditSteps.java
+++ ProjectPhase/Week-02/iMDb/src/test/java/Steps/WatchListEditSteps.java
@@ -5,6 +5,10 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import sun.util.calendar.BaseCalendar;
 
 public class WatchListEditSteps extends BaseUtil {
@@ -17,18 +21,25 @@ public class WatchListEditSteps extends BaseUtil {
 
   @When("^User clicks edit button$")
   public void userClicksEditButton() throws Throwable {
-    
+    base.driver.findElement(By.xpath("//*[@id=\"center-1-react\"]/div/div[1]/div/div[1]/a")).click();
   }
 
   @And("^User is redirected to the edit page$")
   public void userIsRedirectedToTheEditPage() throws Throwable {
+    Assert.assertTrue(base.driver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/button")).isDisplayed());
   }
 
   @And("^User switches the first and the third list element$")
   public void userSwitchesTheFirstAndTheThirdListElement() throws Throwable {
+    WebElement firstInput = base.driver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div[3]/div[5]/div[1]/div[1]/input"));
+    WebElement thirdInput = base.driver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div[3]/div[5]/div[3]/div[1]/input"));
+    firstInput.sendKeys(Keys.BACK_SPACE, "3");
+    thirdInput.sendKeys(Keys.BACK_SPACE, "1");
   }
 
   @Then("^User clicks on Done and the popup$")
   public void userClicksOnDoneAndThePopup() throws Throwable {
+    base.driver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/button")).click();
+    base.driver.findElement(By.id("reorderButton")).click();
   }
 }
