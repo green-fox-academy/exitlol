c5d625d507be5daeb1a1bae13318378794bf2856
exitlol <neutralpointz@gmail.com>
2018-08-09 16:08:37 +0200

    FEAT(refactored WatchListAdd according to POM) -- not working as intended

--- ProjectPhase/Week-02/iMDb/src/test/java/Pages/WatchlistPage.java
+++ ProjectPhase/Week-02/iMDb/src/test/java/Pages/WatchlistPage.java
@@ -3,9 +3,12 @@ package Pages;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class WatchlistPage {
 
@@ -19,11 +22,41 @@ public class WatchlistPage {
   @FindBy(how = How.XPATH, using = "//*[@id=\"center-1-react\"]/div/div[3]/span[2]")
   public WebElement checkWatchlist;
 
+  @FindBy(how = How.XPATH, using = "//a[@href='/chart/top/?ref_=wl_expl_1']")
+  public WebElement topRated;
+
+  @FindBy(how = How.CLASS_NAME, using = "desc")
+  public WebElement onTopRated;
+
+  @FindBy(how = How.XPATH, using = "//div[@class='wl-ribbon standalone not-inWL']")
+  public List<WebElement> addRibbons;
+
+  @FindBy(how = How.XPATH, using = "//*[@id=\"page-1\"]/div")
+  public WebElement notEmptyWatchlistChecker;
+
   public void clickOnWatchlist() {
     userWatchlist.click();
   }
 
   public void verifyEmptyList() {
     Assert.assertTrue(checkWatchlist.isDisplayed());
+  }
+
+  public void clickOnTopRated() {
+    topRated.click();
+  }
+
+  public void topRatedChecker() {
+    Assert.assertTrue(onTopRated.isDisplayed());
+  }
+
+  public void addToWatchlist(int numOfMovies) {
+    for (int i = 0; i < numOfMovies; i ++) {
+      addRibbons.get(i).click();
+    }
+  }
+
+  public void addChecker() {
+    Assert.assertTrue(notEmptyWatchlistChecker.isDisplayed());
   }
 }
--- ProjectPhase/Week-02/iMDb/src/test/java/Steps/AddToWatchListSteps.java
+++ ProjectPhase/Week-02/iMDb/src/test/java/Steps/AddToWatchListSteps.java
@@ -1,6 +1,7 @@
 package Steps;
 
 import Base.BaseUtil;
+import Pages.WatchlistPage;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
@@ -22,40 +23,38 @@ public class AddToWatchListSteps extends BaseUtil {
 
   @Given("^User is already logged in and on watchlist page$")
   public void userIsAlreadyLoggedInAndOnWatchlistPage() throws Throwable {
-    base.driver.findElement(By.xpath("//*[@id=\"navWatchlistMenu\"]/p/a")).click();
+    WatchlistPage page = new WatchlistPage(base.driver);
+    page.clickOnWatchlist();
   }
 
   @When("^User clicks on Top Rated Movies$")
   public void userClicksOnTopRatedMovies() throws Throwable {
-    base.driver.findElement(By.xpath("//a[@href='/chart/top/?ref_=wl_expl_1']")).click();
+    WatchlistPage page = new WatchlistPage(base.driver);
+    page.clickOnTopRated();
   }
 
   @And("^User should see a list of movies$")
   public void userShouldSeeAListOfMovies() throws Throwable {
-    String pageTitle = base.driver.getTitle();
-    if (pageTitle.contains("Top 250")) {
-      System.out.println("User is on desired page!");
-    }
+    WatchlistPage page = new WatchlistPage(base.driver);
+    page.topRatedChecker();
   }
 
   @And("^User adds the first five movies into his/her watchlist$")
   public void userAddsTheFirstFiveMoviesIntoHisHerWatchlist() throws Throwable {
+    WatchlistPage page = new WatchlistPage(base.driver);
     Thread.sleep(2000);
-    for (int i = 1; i < 6; i++) {
-      base.driver.findElement(By.xpath("//*[@id=\"main\"]/div/span/div/div/div[3]/table/tbody/tr[" + i + "]/td[5]/div/div")).click();
-      // Thread.sleep(500);
-    }
+    page.addToWatchlist(5);
   }
 
   @And("^User clicks on own watchlist$")
   public void userClicksOnOwnWatchlist() throws Throwable {
-    base.driver.findElement(By.xpath("//*[@id=\"navWatchlistMenu\"]/p/a")).click();
+    WatchlistPage page = new WatchlistPage(base.driver);
+    page.clickOnWatchlist();
   }
 
   @Then("^User should see his/her not empty watchlist$")
   public void userShouldSeeHisHerNotEmptyWatchlist() throws Throwable {
-    if (base.driver.findElement(By.xpath("//*[@id=\"page-1\"]/div")).isDisplayed()) {
-      System.out.println("User is on watchlist and it's not empty");
-    }
+    WatchlistPage page = new WatchlistPage(base.driver);
+    page.addChecker();
   }
 }
