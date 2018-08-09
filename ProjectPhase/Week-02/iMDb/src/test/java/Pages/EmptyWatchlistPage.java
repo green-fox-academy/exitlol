3bcc7d97aa8b034a6c854d8960b031dc48027eca
exitlol <neutralpointz@gmail.com>
2018-08-09 13:33:51 +0200

    FEAT(Empty watchlist refactored according to POM)

--- ProjectPhase/Week-02/iMDb/src/test/java/Pages/EmptyWatchlistPage.java
+++ ProjectPhase/Week-02/iMDb/src/test/java/Pages/EmptyWatchlistPage.java
@@ -1,6 +1,29 @@
 package Pages;
 
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
 public class EmptyWatchlistPage {
 
-  
+  public EmptyWatchlistPage(WebDriver driver) {
+    PageFactory.initElements(driver, this);
+  }
+
+  @FindBy(how = How.XPATH, using = "//*[@id=\"navWatchlistMenu\"]/p/a")
+  public WebElement userWatchlist;
+
+  @FindBy(how = How.XPATH, using = "//*[@id=\"center-1-react\"]/div/div[3]/span[2]")
+  public WebElement checkWatchlist;
+
+  public void clickOnWatchlist() {
+    userWatchlist.click();
+  }
+
+  public void verifyEmptyList() {
+    Assert.assertTrue(checkWatchlist.isDisplayed());
+  }
 }
--- ProjectPhase/Week-02/iMDb/src/test/java/Steps/EmptyWatchListStep.java
+++ ProjectPhase/Week-02/iMDb/src/test/java/Steps/EmptyWatchListStep.java
@@ -1,6 +1,7 @@
 package Steps;
 
 import Base.BaseUtil;
+import Pages.EmptyWatchlistPage;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
@@ -19,13 +20,13 @@ public class EmptyWatchListStep extends BaseUtil {
 
   @Given("^User clicks on watchlist$")
   public void userClicksOnWatchlist() throws Throwable {
-    base.driver.findElement(By.xpath("//*[@id=\"navWatchlistMenu\"]/p/a")).click();
+    EmptyWatchlistPage page = new EmptyWatchlistPage(base.driver);
+    page.clickOnWatchlist();
   }
 
   @Then("^User should see his/her own empty watchlist$")
   public void userShouldSeeHisHerOwnEmptyWatchlist() throws Throwable {
-    base.driver.findElement(By.xpath("//*[@id=\"center-1-react\"]/div/div[3]/span[2]"));
-    System.out.println("Empty watchlist found.\nTerminating!");
-
+    EmptyWatchlistPage page = new EmptyWatchlistPage(base.driver);
+    page.verifyEmptyList();
   }
 }
